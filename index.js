ed14a75b87f3e38d729c44b14a407ceb1cd70838
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:25:40 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Get started!
+const name = prompt("What's your name?")
+
+console.log("Welcome to zombies and figthers!," + name)
+
+let role = prompt(" Choose your role; fighter or zombie")
+
+console.log(`Okay, ${name}, your role is now ${role}.`)
+
+let level = prompt("What is your level")
+level = parseInt(level)
+
